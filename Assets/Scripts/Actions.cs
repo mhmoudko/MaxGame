ba7d6bc4cfabd2bf150fd9bee7dcb2b1b4368000
mhmoudko <mhmoudko.osama@gmail.com>
2017-09-07 19:59:30 +0200

    MaxGame

---
+++ Assets/Scripts/Actions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Actions : MonoBehaviour {
+
+	public void startGame(){
+		SceneManager.LoadScene ("GAME");
+	}
+
+	public void quitGame(){
+		Application.Quit ();
+	}
+
+	public void backToMainMenu(){
+		SceneManager.LoadScene ("MENU");
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour {
+
+	public Text scoreText;
+	private int scoreCounter = 0;
+	public Camera myCamera;
+	private AudioSource a;
+	public AudioClip collectSound;
+
+	void Start(){
+		a = myCamera.GetComponent<AudioSource> ();
+	}
+
+	void FixedUpdate(){
+		if(this.transform.position.y < -5){
+			SceneManager.LoadScene ("LOSE");
+		}
+	}
+
+	void OnTriggerEnter(Collider x){
+		if(x.gameObject.tag == "pick"){
+			x.gameObject.SetActive (false);
+			scoreCounter++;
+			scoreText.text = "Score: " + scoreCounter;
+			a.PlayOneShot (collectSound);
+			if(scoreCounter == 6){
+				SceneManager.LoadScene ("WIN");
+			}
+		}
+	}
+}
